821544be607b0b4d37420bf752d55f6fec15f7d1
alma <alva2003ro@gmail.com>
2023-10-19 09:15:03 +0200

    nueva clase creada

--- src/baseNoStates/User.java
+++ src/baseNoStates/User.java
@@ -2,13 +2,7 @@ package baseNoStates;
 
 import java.time.LocalTime;
 
-class rol{ //variables globales para cada tipo de user de tipo numerico
-  static final int blank = 0;
-  static final int employee = 1;
 
-  static final int manager = 2;
-  static final int admin = 3;
-}
 
 
 
@@ -16,19 +10,18 @@ public class User {
   private final String name;
   private final String credential;
 
-  private final int role; //rol del usuario dentro de la app
 
-  public User(String name, String credential, int role) {
+
+  public User(String name, String credential) {
     this.name = name;
     this.credential = credential;
-    this.role = role; //modificado
+
   }
 
   public String getCredential() {
     return credential;
   }
 
-  public int getRole(){return role;}
   @Override
   public String toString() {
     return "User{name=" + name + ", credential=" + credential + "}";
