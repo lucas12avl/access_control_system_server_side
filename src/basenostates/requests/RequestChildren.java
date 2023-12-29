9faa9079ba7a8230d7c22e7c2f28a91a65b3c65c
lucas12avl <73055673+lucas12avl@users.noreply.github.com>
2023-12-29 20:34:30 +0100

    //TODO: make a function that return the root id instead of hardcodeit bc what if we change the tree? already done on flutter client but needed minor changes on the server side

--- src/basenostates/requests/RequestChildren.java
+++ src/basenostates/requests/RequestChildren.java
@@ -28,7 +28,14 @@ public class RequestChildren implements Request{
 
     public void process() {
         // TODO What would be the equivalent of getInstance? -- maybe is the unic instance that we have: the root area to search the area with the given areaId
-        Area area = DirectoryAreas.getInstance().findAreaById(areaId);
+        Area area;
+
+        if (areaId.equals("root")){ //all the trees must have a root that gives the first instance bc what if we change the tree?
+            area = DirectoryAreas.getInstance();
+        }
+        else {
+            area = DirectoryAreas.getInstance().findAreaById(areaId);
+        }
         jsonTree = area.toJson(1);
     }
 }
